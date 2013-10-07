b8dc115c279807831460f5697e3147bc992cb734
Brian Han <brianhan87usa@gmail.com>
2013-10-07 12:20:54 -0400

    First commit; Delete public/index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Progress::Application.config.secret_token = 'c6dcb4f2c9ed5c6d98a724e79901d55d292b860f39d8d786813b37e270f97ed5015fe5d7b339da7837782ad046b95baf6b6882a39c33889a45ea56e41077ec7a'
