8b023a23f442a291664265ef3899d46c30495893
Brian Han <brianhan87usa@gmail.com>
2013-10-13 14:03:20 -0400

    Modify users controller to edit/update attributes; new/edit user forms in _forms partial;

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,9 +32,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :profile do
+    process :resize_to_limit => [150, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
