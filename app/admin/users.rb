0317661d0d795c37c57f564331aa0e620fbdad7a
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-17 13:01:16 -0400

    updated Admin/User to make fields sortable

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -6,16 +6,17 @@ ActiveAdmin.register User do
     column :date_of_birth do |user|
       user.date_of_birth.strftime("%b %e, %Y")
     end
-    column :gender do |user|
+    column :gender, :sortable => :gender do |user|
       user.gender.capitalize
     end
     column :points
     column :status
-    column "Date Joined",:created_at do |user|
-      user.created_at.strftime("%b %e, %Y, at %l:%M %p")
+    column "Date Joined",:created_at, :sortable => :created_at do |user|
+      user.created_at.strftime("%b %e, %Y at %l:%M %p")
     end
-    column "Last Updated",:updated_at do |user|
-      user.updated_at.strftime("%b %e, %Y, at %l:%M %p")
+    column "Last Updated", :updated_at, :sortable => :updated_at do |user|
+      user.updated_at.strftime("%b %e, %Y at %l:%M %p")
     end
+    default_actions
   end
 end
