8d8f646c4f5bcadc85a586dd655137d6f19080d7
Brian Han <brianhan87usa@gmail.com>
2013-10-17 00:01:42 -0400

    Layout for User Profile page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :header do 
-    process :resize_to_limit => [1000, 500]
+    process :resize_to_limit => [1000, 1000]
   end
 
   version :profile do
