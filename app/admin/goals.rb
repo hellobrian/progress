2b99d076e4ad3523ab597a8637270a1195ecd72b
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-17 13:23:00 -0400

    set-up Admin/Goal with date and gender formatting

--- app/admin/goals.rb
+++ app/admin/goals.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Goal do
-  
+  index do
+    column :user_id
+    column :verb, :sortable => :verb do |goal|
+      goal.verb.capitalize
+    end
+    column :title
+    column :motivation
+    column :points
+    column "Creation Date",:created_at, :sortable => :created_at do |goal|
+      goal.completion_date.strftime("%b %e, %Y")
+    end
+    column "Last Updated", :updated_at, :sortable => :updated_at do |goal|
+      goal.updated_at.strftime("%b %e, %Y at %l:%M %p")
+    end
+    default_actions
+  end
 end
