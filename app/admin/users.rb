7d8c9a2edc6cfb758142abee10bba41421807e0b
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-17 12:55:37 -0400

    Update Admin/User with format date/time/gender

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :first_name
+    column :last_name
+    column :email
+    column :date_of_birth do |user|
+      user.date_of_birth.strftime("%b %e, %Y")
+    end
+    column :gender do |user|
+      user.gender.capitalize
+    end
+    column :points
+    column :status
+    column "Date Joined",:created_at do |user|
+      user.created_at.strftime("%b %e, %Y, at %l:%M %p")
+    end
+    column "Last Updated",:updated_at do |user|
+      user.updated_at.strftime("%b %e, %Y, at %l:%M %p")
+    end
+  end
 end
